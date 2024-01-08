3e53a9638277e23b36ae79fa6cd2ba6679a05230
bc <costin.botescu@gmail.com>
2024-01-08 11:19:37 +0200

    -

--- inc/gtk.h
+++ inc/gtk.h
@@ -389,7 +389,6 @@ void gtk_window_set_default_size (GtkWindow *window, gint width, gint height);
 void gtk_window_set_icon (GtkWindow *window, GdkPixbuf *icon);
 void gtk_window_set_title (GtkWindow *window,const gchar *title);
 void gtk_window_maximize (GtkWindow *window);
-void gtk_window_unmaximize (GtkWindow *window);
 
 #ifdef __cplusplus
 }
